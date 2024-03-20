5953775be8bc9d01ef4c2b6e29e994182029359b
xah7p <Uglupia@gmail.com>
2024-03-20 15:49:13 +0300

    d

--- 1.py
+++ 1.py
@@ -1,13 +1,17 @@
 import csv
 
+summ = 0
 with open("products.csv", encoding='utf-8') as in_file:
     reader = csv.reader(in_file, delimiter=";")
-    with open("res1.csv", 'w', encoding='utf-8', newline='') as out_file:
+    with open("products_new.csv", 'w', encoding='utf-8', newline='') as out_file:
         writer = csv.writer(out_file, delimiter=";")
         for row in reader:
             print(row)
             if row[0] != "Category":
                 row.append(float(row[3]) * float(row[4]))
+                if row[0] == "Закуски":
+                    summ += row[-1]
             else:
                 row.append("total")
             writer.writerow(row)
+print(summ)
