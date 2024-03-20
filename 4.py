324dd8fbc00e188ad5b1071c3258f35801778b17
xah7p <Uglupia@gmail.com>
2024-03-20 16:48:23 +0300

    d2

--- 4.py
+++ 4.py
@@ -1,5 +1,15 @@
 import csv
 
+'''
+функция make_promo реализует создание промокода для продукта
+ей на вход подаётся список с информацией о продукте в виде: [Category;product;Date;Price per unit;Count]
+функция формирует промокод по следующей формуле:
+    первые 2-ух букв названия + 
+    день поступления + 
+    2 предпоследние буквы названия в обратном порядке + 
+    месяц поступления в обратном порядке.
+'''
+
 
 def make_promo(row):
     _, name, date, _, _ = row
@@ -11,14 +21,16 @@ with open("products.csv", encoding='utf-8') as in_file:
     reader = csv.reader(in_file, delimiter=";")
     with open("product_promo.csv", 'w', newline='', encoding='utf-8') as out_file:
         writer = csv.writer(out_file, delimiter=";")
-        answer = []
+        answer = []  # список, в который будут сохранятся обновлённые строки
         for row in reader:
             if row[0] != "Category":
                 row.append(make_promo(row))
             else:
+                # если это первая строка, то вместо промокода
+                # добавляем название столбца: "promocode"
                 row.append("promocode")
             answer.append(row)
-        writer.writerows(answer)
+        writer.writerows(answer)  # записываем в csv файл
 
 
 
