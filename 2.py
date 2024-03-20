03a0f84ee745f62d3736c05667b2761f7a9667be
xah7p <Uglupia@gmail.com>
2024-03-20 16:03:49 +0300

    d2

--- 2.py
+++ 2.py
@@ -1,17 +1,12 @@
 import csv
 
-summ = 0
 with open("products.csv", encoding='utf-8') as in_file:
     reader = csv.reader(in_file, delimiter=";")
-    with open("products_new.csv", 'w', encoding='utf-8', newline='') as out_file:
-        writer = csv.writer(out_file, delimiter=";")
-        for row in reader:
-            print(row)
-            if row[0] != "Category":
-                row.append(float(row[3]) * float(row[4]))
-                if row[0] == "Закуски":
-                    summ += row[-1]
-            else:
-                row.append("total")
-            writer.writerow(row)
-print(summ)
+    data = []
+    for row in reader:
+        if row[0] != "Category":
+            data.append(row)  # заполняем пустой список data строками с данными о продуктах
+    data.sort(key=lambda x: (x[0], x[3]))  # сортируем по категории товара и цене за единицу товара
+    first = [i for i in data if i[0] == data[0][0]]  # создаем список товаров из первой категории
+    print(f"В категории: {data[0][0]} самый дорогой товар: {first[-1][1]} "
+          f"его цена за единицу товара составляет {first[-1][3]}")
